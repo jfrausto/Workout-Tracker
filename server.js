b84ee2cc226ddb4be159af1a92fcf39eb6a7a2a4
jessef <1319957+jfrausto@users.noreply.github.com>
2020-10-26 01:05:17 -0700

    start Workout model

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+// const db = require("./models");
 
 const app = express();
 
@@ -15,5 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", { useNewUrlParser: true });
 
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });

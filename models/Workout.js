79c5c4199ea9a85f924cbc35c6e197e8e0c79c93
jessef <1319957+jfrausto@users.noreply.github.com>
2020-10-30 17:43:13 -0700

    update Workout model with virtuals; get total duration in home route

--- models/Workout.js
+++ models/Workout.js
@@ -1,3 +1,33 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: { type: Date, default: Date.now },
+    exercises: [{
+        type: { type: String },
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number
+        }]
+  }, {
+      toObject: {virtuals: true},
+      toJSON: {virtuals: true}
+  }
+  );
+  
+  WorkoutSchema.virtual("totalDuration").get(function(){
+    let total = 0;
+    for (let i = 0; i < this.exercises.length; i++){
+        total = total + this.exercises[i].duration;
+    }
+    return total;
+  });
+
+  const Workout = mongoose.model("Workout", WorkoutSchema);
+  
+  module.exports = Workout;
+  
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+let db = require("./models/");
+
+
 const PORT = process.env.PORT || 3000;
 
 // const db = require("./models");
@@ -15,7 +18,24 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find().sort({ _id: -1}).limit(1).exec((err, found) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      console.log(found);
+      res.send(found);
+    }
+  });
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
